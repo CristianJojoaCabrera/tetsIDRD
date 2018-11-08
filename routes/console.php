84b6a7bd0f46f8b94dafbbe18312b1236f3f590b
Unknown <cristianjojoa01@gmail.com>
2018-11-08 12:29:14 -0500

    prueba dos

--- routes/console.php
+++ routes/console.php
@@ -12,7 +12,7 @@ use Illuminate\Foundation\Inspiring;
 | simple approach to interacting with each command's IO methods.
 |
 */
-
+//estamos haciendo pruebas
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->describe('Display an inspiring quote');
